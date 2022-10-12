60e0f803e9bc30f5a4d26d89af74183eb6ae2fe8
user_name <dummtindex@gmail.com>
2022-10-12 11:00:50 -0400

    add pytest

---
+++ tests/track_sort.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+import numpy as np
+import pytest
+import livecell_tracker.track
+
+
+def test_sort_track_consistency():
+    """check if sorting tracks is consistent with the original sort version."""
+    pass
