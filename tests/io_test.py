d70fab7693923f2b95bd582a39c7ed14112f63e3
user_name <dummtindex@gmail.com>
2022-11-01 16:03:13 -0400

    Update io_test.py

--- tests/io_test.py
+++ tests/io_test.py
@@ -13,30 +13,32 @@ def test_posix_paths():
     imgs = LiveCellImageDataset(TestUtils.RAW_DATASET_PATH, ext="png")
     print("dataset path: %s" % (imgs.get_dataset_path()))
     print("dataset name: %s" % (imgs.get_dataset_name()))
-    print("dataset img list: %s" % str(imgs.img_path_list))
-    all_paths_cat = "**".join(imgs.img_path_list)
+    print("dataset img list: %s" % str(imgs.time2path))
+    all_paths_cat = "**".join(imgs.time2path)
     assert all_paths_cat.find("\\") == -1
+
 
 # TODO
 def test_LiveCellImageDataset_json():
     pass
+
 
 # TODO
 def test_SingleCell_json():
     sc = SingleCellStatic()
     pass
 
+
 # TODO
 def test_SingleCellTrajectory_json():
     sc = SingleCellTrajectory()
     pass
 
+
 # TODO
 def test_SingleCellTrajectoryCollection_json():
     sc = SingleCellTrajectoryCollection()
     pass
-
-
 
 
 if __name__ == "__main__":
