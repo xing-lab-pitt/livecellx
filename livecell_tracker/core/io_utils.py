b6fc47a41e8a315c6959869d0aad4ed092f9ede0
user_name <dummtindex@gmail.com>
2022-11-07 15:59:28 -0500

    Update io_utils.py

--- livecell_tracker/core/io_utils.py
+++ livecell_tracker/core/io_utils.py
@@ -13,12 +13,14 @@ def save_png(img: np.array, path: str, mode="L"):
     img : np.array
         image to save
     """
-    img = img.astype(np.uint8)
-    img = Image.fromarray(img, mode=mode)
-    img.save(path)
+    save_general(path=path, img=img, mode=mode)
 
 
-def save_tiff(path: str, img: np.array):
+def save_tiff(path: str, img: np.array, mode="L"):
+    save_general(path=path, img=img, mode=mode)
+
+
+def save_general(path: str, img: np.array, mode="L"):
     """save image to tiff file
 
     Parameters
@@ -28,6 +30,12 @@ def save_tiff(path: str, img: np.array):
     img : np.array
         image to save
     """
-    img = img.astype(np.uint8)
-    img = Image.fromarray(img, mode="L")
+    if np.unique(img).size >= 256:
+        mode = "I"
+        print(
+            "Warning: saving image with more than 256 unique values as 8-bit, use I mode to save as 32-bit signed integer"
+        )
+    else:
+        img = img.astype(np.uint8)
+    img = Image.fromarray(img, mode=mode)
     img.save(path)
