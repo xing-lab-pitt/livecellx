c3e2e6b0e80d671db854ded373c45185b7dd4792
Ke <dummtindex@gmail.com>
2022-12-28 05:47:16 -0500

    update val loop

--- docs/scripts/eval_loop.sh
+++ docs/scripts/eval_loop.sh
@@ -2,8 +2,9 @@
 
 # eval_out_dir="./eval_results/"
 out_threshold=1
+save_dir="./eval_results_out="$out_threshold"_v1-viz_lab3/"
 
-for modelVer in $(seq 76 87)
+for modelVer in $(seq 88 100)
 do
 python ../livecell_tracker/model_zoo/segmentation/eval_csn.py \
     --name="csn_model$modelVer" \
@@ -11,6 +12,19 @@ python ../livecell_tracker/model_zoo/segmentation/eval_csn.py \
     --test_dir="./notebook_results/a549_ccp_vim/test_data_v4"\
     --ckpt="./lightning_logs/version_$modelVer/checkpoints/epoch=999-step=224000.ckpt"\
     --out_threshold=$out_threshold \
-    --save_dir="./eval_results_out="$out_threshold"_v1-metrics_lab3/"\
-    --wait_for_gpu_mem
+    --save_dir=$save_dir\
+    --wait_for_gpu_mem\
+    --viz_pred
 done
+
+# for modelVer in $(seq 76 87)
+# do
+# python ../livecell_tracker/model_zoo/segmentation/eval_csn.py \
+#     --name="csn_model$modelVer" \
+#     --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" \
+#     --test_dir="./notebook_results/a549_ccp_vim/test_data_v4"\
+#     --ckpt="./lightning_logs/version_$modelVer/checkpoints/epoch=999-step=224000.ckpt"\
+#     --out_threshold=$out_threshold \
+#     --save_dir="./eval_results_out="$out_threshold"_v1-metrics_lab3/"\
+#     --wait_for_gpu_mem
+# done
