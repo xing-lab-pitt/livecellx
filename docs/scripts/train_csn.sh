0441c2d20f26379271103e0e2b5e56855d8751dc
Ke <ken67@pitt.edu>
2023-01-01 15:25:01 -0500

    update train_csn script history

--- docs/scripts/train_csn.sh
+++ docs/scripts/train_csn.sh
@@ -207,6 +207,40 @@ nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir
 model=108
 nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_aug_duplicate --class-weights=1,2,2 --model_ckpt="$HOME/LiveCellTracker-dev/notebooks/lightning_logs/version_95/checkpoints/epoch=999-step=224000.ckpt" >train_out_v4_model"$model"_resume.out 2>&1 &
 
+# model 109 (train on slurm cluster)
+# model 96
+# model 83 aug
+# compare with 82
+model=109
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_aug_duplicate --class-weights=1,10,10 --model_ckpt="$HOME/LiveCellTracker-dev/notebooks/lightning_logs/version_96/checkpoints/epoch=999-step=224000.ckpt" >train_out_v4_model"$model"_resume.out 2>&1 &
+
+# model 110 (train on slurm cluster)
+# model 97
+# aug model 86
+# compare with 82
+model=110
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_aug_duplicate --class-weights=1,20,20 --model_ckpt="$HOME/LiveCellTracker-dev/notebooks/lightning_logs/version_97/checkpoints/epoch=999-step=224000.ckpt">train_out_v4_model"$model"_resume.out 2>&1 &
+
+# model 111 (train on slurm cluster)
+# model 98
+# model 84 aug
+# compare with 81
+model=111
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_duplicate --apply_gt_seg_edt --loss=MSE --class-weights=1,2,2 --model_ckpt="$HOME/LiveCellTracker-dev/notebooks/lightning_logs/version_98/checkpoints/epoch=999-step=224000.ckpt">train_out_v4_model"$model"_resume.out 2>&1 &
+
+# model 112 (train on slurm cluster)
+# model 99
+# model 85 aug
+# compare with 84
+model=112
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_duplicate --apply_gt_seg_edt --loss=MSE --class-weights=1,10,10 >train_out_v4_model"$model"_resume.out 2>&1 &
+
+# model 113 (train on slurm cluster)
+# model 100
+# model 87 aug (not yet trained)
+model=113
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_duplicate --apply_gt_seg_edt --loss=MSE --class-weights=1,20,20 >train_out_v4_model"$model"_resume.out 2>&1 &
+
 ############################################################################################################
 # start from 200: replace CE with BCE (retrain previous default arg models)
 ############################################################################################################
