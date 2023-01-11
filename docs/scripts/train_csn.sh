97563532c717269825ebad9eae1c6d79616e262d
Ke <ken67@pitt.edu>
2023-01-11 17:44:12 -0500

    update train_csn scripts

--- docs/scripts/train_csn.sh
+++ docs/scripts/train_csn.sh
@@ -288,6 +288,30 @@ nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir
 model=206
 nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_aug_duplicate --loss=BCE  >train_out_v4_model"$model"_resume.out 2>&1 &
 
+
+# model 208
+# model 108 (train on slurm cluster)
+# model 95
+# model 82 aug
+# compare with 75
+model=208
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_aug_duplicate --class-weights=1,2,2 --loss=BCE >train_out_v4_model"$model"_resume.out 2>&1 &
+
+# model 209
+# model 109 (train on slurm cluster)
+# model 96
+# model 83 aug
+# compare with 82
+model=209
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_aug_duplicate --class-weights=1,10,10 --loss=BCE >train_out_v4_model"$model"_resume.out 2>&1 &
+
+# model 210 (train on slurm cluster)
+# model 97
+# aug model 86
+# compare with 82
+model=210
+nohup python ../livecell_tracker/model_zoo/segmentation/train_csn.py --train_dir="./notebook_results/a549_ccp_vim/train_data_v4" --model_version=version_$model --epochs=2000 --kernel_size=1 --batch_size=2 --degrees=180 --translation=0.5 --aug_scale="0.5,2" --input_type=raw_aug_duplicate --class-weights=1,20,20 --loss=BCE >train_out_v4_model"$model"_resume.out 2>&1 &
+
 ############################################################################################################
 # draft below
 ############################################################################################################
