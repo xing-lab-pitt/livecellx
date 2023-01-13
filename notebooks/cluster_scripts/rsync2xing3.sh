aef22c380391181212257f333815e82bebab877b
dummyindex <dummtindex@gmail.com>
2023-01-13 17:00:48 -0500

    update resync script

--- notebooks/cluster_scripts/rsync2xing3.sh
+++ notebooks/cluster_scripts/rsync2xing3.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+# to workstation 3
 rsync -rav --progress --ignore-existing ./lightning_logs/version_* ken67@xing-lab-3.csb.pitt.edu:/home/ken67/LiveCellTracker-dev/notebooks/lightning_logs/
 
-rsync -rav --progress --size-only ken67@xing-lab-3.csb.pitt.edu:/home/ken67/LiveCellTracker-dev/notebooks/lightning_logs/ ./lightning_logs/
+# to pitt cluster
+rsync -rav --progress --ignore-existing ./lightning_logs/version_* ken67@cluster.csb.pitt.edu:/net/capricorn/home/xing/ken67/LiveCellTracker-dev/notebooks/lightning_logs/
+
+# from workstation 3
+rsync -rav --progress --ignore-existing ken67@xing-lab-3.csb.pitt.edu:/home/ken67/LiveCellTracker-dev/notebooks/lightning_logs/ ./lightning_logs/
+
+# from pitt cluster
+rsync -rav --progress --ignore-existing ken67@cluster.csb.pitt.edu:/net/capricorn/home/xing/ken67/LiveCellTracker-dev/notebooks/lightning_logs/ ./lightning_logs/
