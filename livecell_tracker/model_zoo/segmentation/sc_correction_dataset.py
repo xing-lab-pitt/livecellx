f0c9d3ea524aaab9e291a478337718fe6e6aa959
Ke <dummtindex@gmail.com>
2023-02-01 00:41:32 -0500

    fix a bug

--- livecell_tracker/model_zoo/segmentation/sc_correction_dataset.py
+++ livecell_tracker/model_zoo/segmentation/sc_correction_dataset.py
@@ -145,7 +145,8 @@ class CorrectSegNetDataset(torch.utils.data.Dataset):
             raise NotImplementedError
 
         if self.exclude_raw_input_bg:
-            bg_mask = augmented_raw_img == 0
+            # Note that the augmented image has all -1 values for pixels not inside the cell
+            bg_mask = augmented_raw_img <= 0
             input_img[:, bg_mask] = 0
 
         input_img = input_img.float()
