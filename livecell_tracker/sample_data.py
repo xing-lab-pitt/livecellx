da094d3905b1e1ec11f550fbba604b67d861bb4e
Ke <dummtindex@gmail.com>
2023-05-15 14:10:26 -0400

    update sample_data.py

--- livecell_tracker/sample_data.py
+++ livecell_tracker/sample_data.py
@@ -1,11 +1,47 @@
 from pathlib import Path
+import os
+import ntpath
+import urllib
+from urllib.request import urlretrieve
 from livecell_tracker.core.datasets import LiveCellImageDataset, SingleImageDataset
+from livecell_tracker.livecell_logger import LoggerManager, main_info
+
+DEFAULT_DATA_DIR = Path("./datasets")
 
 
-def tutorial_three_image(
+# TODO: use AWS S3 bucket to store the data instead
+def tutorial_three_image_sys(
     dic_dataset_path=Path("./datasets/test_data_STAV-A549/DIC_data"),
     mask_dataset_path=Path("./datasets/test_data_STAV-A549/mask_data"),
+    url="https://pitt-my.sharepoint.com/:u:/g/personal/ken67_pitt_edu/EegFUrWemsNEuNHdx6l_eZ4BT1WHhmbF0YWr5jSkTFICdg?download=1",
+    dir=None,
 ):
+    if dir is None:
+        dir = DEFAULT_DATA_DIR
+    download_data(url, filename="test_data_STAV-A549", dir=DEFAULT_DATA_DIR)
     mask_dataset = LiveCellImageDataset(mask_dataset_path, ext="png")
     dic_dataset = LiveCellImageDataset(dic_dataset_path, ext="tif")
     return dic_dataset, mask_dataset
+
+
+def download_data(url, filename=None, dir=DEFAULT_DATA_DIR):
+    filepath = ntpath.basename(url) if filename is None else filename
+    filepath = os.path.join(dir, filepath)
+    main_info("Downloading data to " + filepath)
+
+    if not os.path.exists(filepath):
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+
+        # download the data
+        opener = urllib.request.build_opener()
+        opener.addheaders = [
+            (
+                "User-agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+            )
+        ]
+        urllib.request.install_opener(opener)
+        urlretrieve(url, filepath, reporthook=LoggerManager.get_main_logger().request_report_hook)
+
+    return filepath
