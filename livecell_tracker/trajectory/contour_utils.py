30c2d7ae0d833e048dc51111b6a5967e9fc61c62
Yajushi Khurana <91340164+yajushikhurana@users.noreply.github.com>
2023-06-14 15:25:55 -0400

    Update contour_utils.py
    added get_morphology_PCA function

--- livecell_tracker/trajectory/contour_utils.py
+++ livecell_tracker/trajectory/contour_utils.py
@@ -6,6 +6,8 @@ from livecell_tracker.core import (
 )
 from livecell_tracker.trajectory.contour.contour_class import Contour
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import numpy as np
 
 
 def get_cellTool_contour_points(traj: SingleCellTrajectory, contour_num_points=500) -> List[Contour]:
@@ -25,3 +27,31 @@ def viz_contours(cell_contours: List[Contour], **kwargs):
     for contour in cell_contours:
         plt.plot(contour.points[:, 0], contour.points[:, 1], **kwargs)
     plt.show()
+    
+def get_morphology_PCA(trajectory_collection, contour_num_points, trajectory_threshold = 1):
+    flat_contour_entire_dataset = []
+
+    for track_id_num in trajectory_collection.get_track_ids():
+        traj = trajectory_collection.get_trajectory(track_id_num)
+
+        # getting cell contours using active shape model
+        cell_contours = get_cellTool_contour_points(traj, contour_num_points=contour_num_points)
+        
+        if len(cell_contours) > trajectory_threshold:
+            # Flatten each 2D array and stack them horizontally
+            flattened_contour = [contour.points.flatten() for contour in cell_contours]
+            final_contour = np.hstack(flattened_contour)
+            flat_contour_entire_dataset.append(final_contour)
+
+    countour_entire_dataset = np.concatenate(flat_contour_entire_dataset, axis = 0).reshape(-1, 2*contour_num_points)
+    
+
+    # getting PCA
+    pca = PCA(n_components=0.98, svd_solver="full")
+
+    pca_entire = pca.fit_transform(countour_entire_dataset)
+    print("Variance ratios and their sum = ", pca.explained_variance_ratio_, sum(pca.explained_variance_ratio_))
+
+    pca_individual_list = [pca.transform(final_contour.reshape((int(len(final_contour)/(2*contour_num_points)), 2*contour_num_points))) for final_contour in flat_contour_entire_dataset]
+
+    return pca_individual_list
