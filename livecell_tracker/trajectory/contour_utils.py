e052e0a1df7181198cd5ddf1b1bb2c8c297fd55a
Yajushi Khurana <91340164+yajushikhurana@users.noreply.github.com>
2023-06-26 11:22:52 -0400

    Update contour_utils.py

--- livecell_tracker/trajectory/contour_utils.py
+++ livecell_tracker/trajectory/contour_utils.py
@@ -28,7 +28,7 @@ def viz_contours(cell_contours: List[Contour], **kwargs):
         plt.plot(contour.points[:, 0], contour.points[:, 1], **kwargs)
     plt.show()
     
-def get_morphology_PCA(trajectory_collection, contour_num_points, trajectory_threshold=1, num_components=0.98):
+def get_morphology_PCA(trajectory_collection, contour_num_points, trajectory_threshold=1, num_components=0.98, dim = 2):
     """Obtain PCA for the morphology contour features obtained through active shape model for an entire trajectory data for an image dataset. 
 
     Args:
@@ -38,35 +38,35 @@ def get_morphology_PCA(trajectory_collection, contour_num_points, trajectory_thr
         num_components (float, optional): the amount of variance that needs to be explained is greater than the percentage specified by num_components.
 
     Returns:
-        List: PCA values for contours
+        List: PCA values for sct contours
     """
-    flat_contour_entire_dataset = []
+    all_flat_contours = [] # all flattened contours, shape: # num_trajectories x (#timeframes * #contour_num_points)
 
     for track_id_num in trajectory_collection.get_track_ids():
         traj = trajectory_collection.get_trajectory(track_id_num)
 
         # getting cell contours using active shape model
-        cell_contours = get_cellTool_contour_points(traj, contour_num_points=contour_num_points)
+        sct_contours = get_cellTool_contour_points(traj, contour_num_points=contour_num_points) #shape: #time x #contours
 
-        if len(cell_contours) > trajectory_threshold:
+        if len(sct_contours) > trajectory_threshold:
             # Flatten each 2D array and stack them horizontally
-            flattened_contour = [contour.points.flatten() for contour in cell_contours]
+            flattened_contour = [contour.points.flatten() for contour in sct_contours]
             final_contour = np.hstack(flattened_contour)
-            flat_contour_entire_dataset.append(final_contour)
+            all_flat_contours.append(final_contour)
 
-    countour_entire_dataset = np.concatenate(flat_contour_entire_dataset, axis=0).reshape(-1, 2 * contour_num_points)
+    all_sctc_contours = np.concatenate(all_flat_contours, axis=0).reshape(-1, dim * contour_num_points)
 
     # getting PCA
-    pca = PCA(n_components=num_components, svd_solver="full")
+    _pca_model = PCA(n_components=num_components, svd_solver="full")
 
-    pca_entire = pca.fit_transform(countour_entire_dataset)
-    print("Variance ratios and their sum = ", pca.explained_variance_ratio_, sum(pca.explained_variance_ratio_))
+    transformed_pca_contour_data = _pca_model.fit_transform(all_sctc_contours)
+    print("Variance ratios and their sum = ", _pca_model.explained_variance_ratio_, sum(_pca_model.explained_variance_ratio_))
 
-    pca_individual_list = [
-        pca.transform(
-            final_contour.reshape((int(len(final_contour) / (2 * contour_num_points)), 2 * contour_num_points))
+    pca_sctc = [
+        _pca_model.transform(
+            final_contour.reshape((int(len(final_contour) / (dim * contour_num_points)), dim * contour_num_points))
         )
-        for final_contour in flat_contour_entire_dataset
+        for final_contour in all_flat_contours
     ]
 
-    return pca_individual_list
+    return pca_sctc
