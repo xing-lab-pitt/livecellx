4ef217e330834973c5105c2ac69fe7af1daeaea3
Ke <dummtindex@gmail.com>
2023-08-15 16:32:05 -0400

    fix a test_posix_paths case string join bug

--- tests/io_test.py
+++ tests/io_test.py
@@ -5,16 +5,19 @@ from pathlib import PurePosixPath, PureWindowsPath
 
 
 def test_posix_pathlib_convert_windows_path():
-    p = PureWindowsPath("c:\\windows")
-    assert p.as_posix() == "c:/windows", "pathlib bug? correct: c:/windows, actual: %s" % p.as_posix()
+    sample_path = PureWindowsPath("c:\\windows")
+    assert sample_path.as_posix() == "c:/windows", (
+        "pathlib bug? correct: c:/windows, actual: %s" % sample_path.as_posix()
+    )
 
 
 def test_posix_paths():
     imgs = LiveCellImageDataset(TestUtils.RAW_DATASET_PATH, ext="png")
+    print(">>> debug, imgs:", imgs.get_dataset_name(), imgs.get_dataset_path())
     print("dataset path: %s" % (imgs.get_dataset_path()))
     print("dataset name: %s" % (imgs.get_dataset_name()))
     print("dataset img list: %s" % str(imgs.time2url))
-    all_paths_cat = "**".join(imgs.time2url)
+    all_paths_cat = "**".join(imgs.time2url.values())
     assert all_paths_cat.find("\\") == -1
 
 
