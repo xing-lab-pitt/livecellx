9ef8236ecbdbb5d428092df56ba7cb8d48adaea7
Ke <dummtindex@gmail.com>
2023-09-13 12:22:40 -0400

    update .readthedocs.yaml and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(module_path))
 sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("../../"))
 
-import livecellx
+# import livecellx
 
 # HERE = Path(__file__).parent
 # sys.path[:0] = [str(HERE.parent)]
