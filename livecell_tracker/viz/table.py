ce90da549335cbb00f7675ab65dde33ae5e60124
MandyZhou807 <101478556+MandyZhou807@users.noreply.github.com>
2023-09-13 01:30:06 -0400

    Add Sc Sct Displayer (#62)
    * Moved notebooks to submodule under docs
    
    * Moved notebooks to submodule under docs
    
    * Revert "Moved notebooks to submodule under docs"
    
    This reverts commit fa4ac753772366dec61e2a7bf73f8d16ab446183.
    
    * Moved livecellx_notebooks into source directory
    
    * Create docs for package
    
    * remove api and class folder
    
    * refine load&save in SingleCellTrajectory to store dataset_json_path in meta
    
    * update test scripts accordingly with s&l method
    
    * Displayer for sc, sct and sctc
    
    * Restore single_cell.py from remote repository
    
    * tests for delete_selected_sct and save_annotations
    
    * remove old ScDisplayer
    
    * demo notebook for displayer
    
    * update displayer.py: remove Annotated
    
    * update tutorial_displayer.ipynb: remove Annotated
    
    * update python workflow
    
    * feat: create csn_cellpose_cxa_wrongful_example-9-11-23.ipynb
    
    * update sct_operator.py: add docs for restore shape layer
    
    * test: update sct_operator_test.py: skip the test
    
    ---------
    
    Co-authored-by: Ke <dummtindex@gmail.com>

---
+++ livecell_tracker/viz/table.py
@@ -0,0 +1,64 @@
+from magicgui import magicgui
+from magicgui.widgets import Table
+import pandas as pd
+from livecell_tracker.core import SingleCellStatic, SingleCellTrajectory
+from livecell_tracker.trajectory.feature_extractors import compute_skimage_regionprops
+
+
+@magicgui
+def sc_static_table_widget(sc: SingleCellStatic):
+    # Create a DataFrame from SingleCellStatic instance
+    data = {
+        "Timeframe": sc.timeframe,
+        "BoundingBox": sc.bbox,
+        "Centroid": sc.regionprops.centroid,
+        "Dataset Name": sc.img_dataset.get_dataset_name(),
+    }
+    # Convert to DataFrame
+    df = pd.DataFrame(list(data.items()), columns=["Attribute", "Value"])
+
+    # Add the feature_dict as separate rows
+    feature_dict = sc.get_feature_pd_series().to_dict()
+    feature_df = pd.DataFrame(list(feature_dict.items()), columns=["Attribute", "Value"])
+    df = pd.concat([df, feature_df], ignore_index=True)
+
+    # Create a Table widget and populate it with the DataFrame
+    table = Table(value=df)
+    return table
+
+
+@magicgui
+def sct_table_widget(trajectory: SingleCellTrajectory):
+    # Get feature table from SingleCellTrajectory
+    trajectory.compute_features("skimage", compute_skimage_regionprops)
+    feature_table = trajectory.get_sc_feature_table()
+
+    # Create a DataFrame to store the attributes and features
+    data = {
+        "Timeframe": [],
+        "BoundingBox": [],
+        "Centroid": [],
+        "Dataset Name": [],
+    }
+
+    # Initialize the feature columns with empty lists
+    for feature_key in feature_table.columns:
+        data[feature_key] = []
+
+    # Iterate through the SingleCellStatic instances in the trajectory
+    for timeframe, single_cell in trajectory.timeframe_to_single_cell.items():
+        data["Timeframe"].append(timeframe)
+        data["BoundingBox"].append(single_cell.bbox)
+        data["Centroid"].append(single_cell.regionprops.centroid)
+        data["Dataset Name"].append(single_cell.img_dataset.get_dataset_name())
+
+        # Extract features from the feature table and add to data
+        row_index = "_".join([str(trajectory.track_id), str(timeframe)])
+        for feature_key in feature_table.columns:
+            data[feature_key].append(feature_table.at[row_index, feature_key])
+
+    df = pd.DataFrame(data)
+
+    # Create a Table widget and populate it with the DataFrame
+    table = Table(value=df)
+    return table
