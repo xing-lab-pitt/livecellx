dd2e267bf54a0cb78771fa9e4d2b90584e91e619
dummyindex <dummtindex@gmail.com>
2024-01-10 01:25:31 -0500

    relocate v13 eval scripts and create v13 eval on v14 normal samples

---
+++ notebooks/scripts/mmdetection_classify/local_eval_scripts/v13/eval_timesformer-default-divst-v13-drop-div-combined-random-crop-sequential.sh
@@ -0,0 +1,18 @@
+
+model="timesformer-default-divst-v13-drop-div-combined-random-crop-sequential"
+model_dir="./work_dirs/"$model
+video_dir="../../notebook_results/mmaction_train_data_v13-inclusive-corrected/videos"
+test_data_meta_path="../../notebook_results/mmaction_train_data_v13-inclusive-corrected/test_data.txt"
+
+python classify_mmdetection_mitosis_eval.py \
+    --model_dir=$model_dir \
+    --out_dir="./work_dirs/eval_results/"$model \
+    --config=$model_dir/"config_train_timesformer_divst_v13-drop-div-combined-random-crop-sequential.py" \
+    --checkpoint=$model_dir/"best_acc_top1_epoch_26.pth" \
+    --video_dir=$video_dir \
+    --mmaction_data_tsv=$mmaction_data_tsv \
+    --test_data_meta_path=$test_data_meta_path \
+    --add-random-crop \
+    --device="cuda:1"
+
+
---
+++ notebooks/scripts/mmdetection_classify/local_eval_scripts/v13/eval_tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb-v13-drop-div-video-clipLen=2-trainClipNum=3-valClipNum=3.sh
@@ -0,0 +1,22 @@
+
+model="tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb-v13-drop-div-video-clipLen=2-trainClipNum=3-valClipNum=3"
+model_dir="./work_dirs/"$model
+video_dir="../../notebook_results/mmaction_train_data_v13-inclusive-corrected/videos"
+
+test_data_meta_path="../../notebook_results/mmaction_train_data_v13-inclusive-corrected/test_data.txt"
+
+python classify_mmdetection_mitosis_eval.py \
+    --model_dir=$model_dir \
+    --out_dir="./work_dirs/eval_results/"$model \
+    --config=$model_dir/"config_train_v13_div_cliplen=2_clipnum=3-video.py" \
+    --checkpoint=$model_dir/"best_acc_top1_epoch_11.pth" \
+    --video_dir=$video_dir \
+    --mmaction_data_tsv=$mmaction_data_tsv \
+    --test_data_meta_path=$test_data_meta_path \
+    --device="cuda:1" \
+    --add-random-crop \
+    --is-tsn \
+    --no-wrong-video
+
+
+
