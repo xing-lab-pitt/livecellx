fix: convert augmented_scaled_seg_mask to CPU tensor before normalization and update edt_max parameter

livecellx/model_zoo/segmentation/sc_correction_dataset.py
@@ -237,7 +237,11 @@ class CorrectSegNetDataset(torch.utils.data.Dataset):
         elif self.input_type == "edt_v0":
             # TODO edt transform already done before the transform
             # augmented_scaled_seg_mask = scipy.ndimage.distance_transform_edt(augmented_scaled_seg_mask)
-            augmented_scaled_seg_mask = normalize_edt(augmented_scaled_seg_mask, edt_max=4)
+            if isinstance(augmented_scaled_seg_mask, torch.Tensor):
+                if augmented_scaled_seg_mask.is_cuda:
+                    augmented_scaled_seg_mask = augmented_scaled_seg_mask.cpu()
+                augmented_scaled_seg_mask = augmented_scaled_seg_mask.numpy()
+            augmented_scaled_seg_mask = torch.tensor(normalize_edt(augmented_scaled_seg_mask, edt_max=5))
             input_img = torch.stack(
                 [augmented_raw_transformed_img, augmented_raw_transformed_img, augmented_scaled_seg_mask], dim=0
             )